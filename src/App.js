707d5b685f713fff0d80d18b896ab14356de41c8
sujanig <sujani@ezeu.in>
2022-05-05 23:07:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PerodicPoll from './PeriodicPoll/Assgn3'
+function App() {
+  return (
+    <div className="App">
+      <h2> Periodic Polling</h2>
+      <PerodicPoll/>
+          </div>
+  );
+}
+
+export default App;
---
+++ src/PeriodicPoll/Assgn3.js
@@ -0,0 +1,199 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from '@mui/material/styles';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableHead from '@mui/material/TableHead';
+
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
+
+const url = "https://hn.algolia.com/api/v1/search_by_date";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+  
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+  
+    const handleFirstPageButtonClick = (event) => {
+      onPageChange(event, 0);
+    };
+  
+    const handleBackButtonClick = (event) => {
+      onPageChange(event, page - 1);
+    };
+  
+    const handleNextButtonClick = (event) => {
+      onPageChange(event, page + 1);
+    };
+  
+    const handleLastPageButtonClick = (event) => {
+      onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+    };
+  
+    return (
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+        <IconButton
+          onClick={handleFirstPageButtonClick}
+          disabled={page === 0}
+          aria-label="first page"
+        >
+          {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+        </IconButton>
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 0}
+          aria-label="previous page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+        <IconButton
+          onClick={handleLastPageButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="last page"
+        >
+          {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+        </IconButton>
+      </Box>
+    );
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+  };
+
+export default function Assgn31() {
+    const [post, setpost] = useState([]);
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const emptyRows =
+  page > 0 ? Math.max(0, (1 + page) * rowsPerPage - post.length) : 0;
+
+const handleChangePage = (event, newPage) => {
+  setPage(newPage);
+};
+
+const handleChangeRowsPerPage = (event) => {
+  setRowsPerPage(parseInt(event.target.value, 10));
+  setPage(0);
+}; 
+    const getPost = () => axios
+        .get(url)
+        .then((res) => {
+            
+            setpost([...post,...res.data.hits]);
+        })
+        .catch((err) => {
+        }, []);
+
+    useEffect(() => {
+       
+            const interval = setInterval(() => {
+                getPost()
+            }, 10000)
+           
+            return () => clearInterval(interval)
+       
+    }, [])
+    return (
+        <TableContainer component={Paper}>
+         <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+            <TableHead>
+              <StyledTableRow>
+                <StyledTableCell><h3>Author</h3></StyledTableCell>
+                <StyledTableCell ><h3>URL...</h3></StyledTableCell>
+                <StyledTableCell ><h3>Title</h3></StyledTableCell>
+              </StyledTableRow>
+            </TableHead>
+
+           <TableBody>
+          {(rowsPerPage > 0
+            ? post.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : post
+          ).map((row) => (
+            <StyledTableRow key={row.objectID}>
+              <TableCell >
+              {row.author}
+              </TableCell>
+              <TableCell onClick={()=> window.open("https://hn.algolia.com/api/v1/search_by_date?query=story&page=1", "_blank")}>
+              {row.url}
+              </TableCell>
+              <TableCell >
+              {row.title}
+              </TableCell>
+            </StyledTableRow>
+          ))}
+
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              colSpan={3}
+              count={post.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  'aria-label': 'rows per page',
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+          </Table>
+        </TableContainer>
+      );
+  
+   
+}
